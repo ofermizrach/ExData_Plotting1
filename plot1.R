5cc3fd84e4d77a6c8699c55d60f622c7cd4e5683
ofermizrach <ofer.mizrach@gmail.com>
2015-04-07 15:22:45 -0700

    minor fixes

--- plot1.R
+++ plot1.R
@@ -16,9 +16,14 @@ data_subset <- subset(data, (as.Date(data[,"Date"],format = "%d/%m/%Y")== date1 
 
 data_subset$datetime <- strptime(paste(data_subset$Date, data_subset$Time),format = "%d/%m/%Y %H:%M:%S", tz = "GMT")
 png("plot1.png", width=480, height=480)
-bins <- cut_interval(data_subset$Global_active_power,length=0.5 )
-barplot(table(bins),main = "Global Active Power",col=c("red"), space=0,
-        xlab = "Global Active Power (kilowatts)", ylab= "Frequency")
+#bins <- cut_interval(data_subset$Global_active_power,length=0.5 )
+#barplot(table(bins),main = "Global Active Power",col=c("red"), space=0,
+#        xlab = "Global Active Power (kilowatts)", ylab= "Frequency")
+
+# plot data
+hist(data_subset$Global_active_power,main='Global Active Power',
+     xlab='Global Active Power (kilowatts)',col='red')
+
 #axis(1, labels = c(0,2,4,6))
 dev.off()
 
